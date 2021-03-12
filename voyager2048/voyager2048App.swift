757a3d3596e4b547ee2123eb22f3b587582b4805
grapeVine <treeui.old@gmail.com>
2021-03-12 08:29:53 +0800

    Initial Commit

---
+++ voyager2048/voyager2048App.swift
@@ -0,0 +1,20 @@
+//
+//  voyager2048App.swift
+//  voyager2048
+//
+//  Created by grapestree on 2021/3/12.
+//
+
+import SwiftUI
+
+@main
+struct voyager2048App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
