1de0f0dfe22ed06b72439032d98783d467c56517
grapeVine <treeui.old@gmail.com>
2021-07-10 18:13:49 +0800

    format code

--- voyager2048/ContentView.swift
+++ voyager2048/ContentView.swift
@@ -15,16 +15,24 @@ struct GraperyApp: View {
             NavigationView {
                 TabView(selection: $selection) {
                     UserUIView()
-                        .tabItem { Item(type: .You, selection: selection) }
+                        .tabItem {
+                            Item(type: .You, selection: selection)
+                        }
                         .tag(ItemType.You.rawValue)
                     ExploreUIView()
-                        .tabItem { Item(type: .Explore, selection: selection) }
+                        .tabItem {
+                            Item(type: .Explore, selection: selection)
+                        }
                         .tag(ItemType.Explore.rawValue)
                     GroupUIView()
-                        .tabItem { Item(type: .Org, selection: selection) }
+                        .tabItem {
+                            Item(type: .Org, selection: selection)
+                        }
                         .tag(ItemType.Org.rawValue)
                     FriendsUIView()
-                        .tabItem { Item(type: .Friends, selection: selection) }
+                        .tabItem {
+                            Item(type: .Friends, selection: selection)
+                        }
                         .tag(ItemType.Friends.rawValue)
                 }
                 .navigationBarHidden(itemType.isNavigationBarHidden(selection: selection))
@@ -43,28 +51,40 @@ struct GraperyApp: View {
             
             var image: Image {
                 switch self {
-                case .You:     return Image(".you")
-                case .Explore:  return Image(".explore")
-                case .Org: return Image(".group")
-                case .Friends:       return Image(".friends")
+                case .You:
+                    return Image(".you")
+                case .Explore:
+                    return Image(".explore")
+                case .Org:
+                    return Image(".group")
+                case .Friends:
+                    return Image(".friends")
                 }
             }
             
             var selectedImage: Image {
                 switch self {
-                case .You:     return Image(".you_selected")
-                case .Explore:  return Image("explore_selected")
-                case .Org: return Image(".group_selected")
-                case .Friends:       return Image(".friends_selected")
+                case .You:
+                    return Image(".you_selected")
+                case .Explore:
+                    return Image("explore_selected")
+                case .Org:
+                    return Image(".group_selected")
+                case .Friends:
+                    return Image(".friends_selected")
                 }
             }
             
             var title: String {
                 switch self {
-                case .You:              return "You"
-                case .Explore:          return "Explore"
-                case .Org:              return "Org"
-                case .Friends:          return "Friends"
+                case .You:
+                    return "You"
+                case .Explore:
+                    return "Explore"
+                case .Org:
+                    return "Org"
+                case .Friends:
+                    return "Friends"
                 }
             }
             
@@ -97,11 +117,12 @@ struct GraperyApp: View {
                     } else {
                         type.image
                     }
-                    
                     Text(type.title)
                 }
             }
         }
         
-        private var itemType: ItemType { ItemType(rawValue: selection)! }
+        private var itemType: ItemType {
+            ItemType(rawValue: selection)!
+        }
     }
